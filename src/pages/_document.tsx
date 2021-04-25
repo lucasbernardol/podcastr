7fa9a4b62d0ffc70a19a80fd7103d68edc93b842
= <=>
2021-04-25 10:43:43 -0300

    :tada: Initial commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { Head, Html, NextScript, Main } from 'next/document';
+
+class DOM extends Document {
+	render() {
+		return (
+			<Html lang="pt-br" translate="yes">
+				<Head>
+					<meta charSet="utf-8" />
+					<link rel="shortcut icon" href="/favicon.png" />
+
+					<link rel="preconnect" href="https://fonts.gstatic.com" />
+					<link 
+						href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" 
+						rel="stylesheet" 
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+};
+
+export default DOM;
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+   	<>
+   		<Head>
+   			<title>Início | Podcastr</title>
+   		</Head>
+
+   		<h1>NextJS</h1>
+   	</>
+  );
+}
